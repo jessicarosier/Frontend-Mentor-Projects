a803ca4e6e9c901d83cb42b533b499ae9653270b
Jessica Rosier <jessica.c.rosier1@gmail.com>
2024-01-19 11:09:11 -0600

    age calc complete

--- age-calc-app/js/main.js
+++ age-calc-app/js/main.js
@@ -1,18 +1,16 @@
 let day = document.querySelector("#day-input");
 let month = document.querySelector("#month-input");
 let year = document.querySelector("#year-input");
-let form = document.querySelector("form");
-let dayError = document.querySelector("#day-error");
+let currentYear;
+let errorMessage = "";
 
-let dateString;
 
 let circleArrow = document.querySelector(".circle");
 
 
-
 function calculateAge() {
     let date = new Date();
-    let currentYear = date.getFullYear();
+    currentYear = date.getFullYear();
     let currentMonth = date.getMonth() + 1;
     let currentDay = date.getDate();
     let ageYears = currentYear - year.value;
@@ -36,25 +34,90 @@ function calculateAge() {
     days.innerText = ageDays;
 }
 
-function isValidDate() {
-    // yyyy-mm-dd
-    let validDate = new Date(`${year.value}-${month.value}-${day.value}`);
-    console.log(validDate);
+let error;
+function generateErrorMessage(element, message) {
+    if (element.parentElement.querySelector(".error")) {
+        element.parentElement.querySelector(".error").remove();
+    }
 
+    if (element.classList.contains("invalid")) {
+        element.classList.remove("invalid");
+    }
+    error = document.createElement("p");
+    error.innerText = message;
+    error.classList.add("error");
+    element.parentElement.appendChild(error);
 }
 
-isValidDate();
+function validateInput() {
+    let dayValid = false;
+    let monthValid = false;
+    let yearValid = false;
+    let allInputsValid = false;
 
+    if (parseInt(day.value) > 31 || parseInt(day.value) < 1) {
+        dayValid = false;
+        generateErrorMessage(day, "Must be a valid day");
+    } else if (day.value.length === 0) {
+        dayValid = false;
+        generateErrorMessage(day, "This field is required");
+    } else {
+        dayValid = true;
+        error.remove();
+    }
 
-form.addEventListener("input", function (e) {
-    e.preventDefault();
+    if (parseInt(month.value) > 12 || parseInt(month.value) < 1) {
+        monthValid = false;
+        generateErrorMessage(month, "Must be a valid month");
+    } else if (month.value.length === 0) {
+        monthValid = false;
+        generateErrorMessage(month, "This field is required");
+    } else {
+        monthValid = true;
+        error.remove();
+    }
+    if (parseInt(year.value) > currentYear || parseInt(year.value) < 1) {
+        yearValid = false;
+        generateErrorMessage(year, "Must be in the past");
+    } else if (year.value.length === 0) {
+        yearValid = false;
+        generateErrorMessage(year, "This field is required");
+    } else {
+        yearValid = true;
+        error.remove();
+    }
 
-   isValidDate()
+    if (!dayValid) {
+        day.classList.add("invalid")
+    } else {
+        day.classList.remove("invalid")
+    }
 
-});
+    if (!monthValid) {
+        month.classList.add("invalid")
+    } else {
+        month.classList.remove("invalid")
+    }
+
+    if (!yearValid) {
+        year.classList.add("invalid")
+    } else {
+        year.classList.remove("invalid")
+    }
+
+    if (dayValid && monthValid && yearValid) {
+        allInputsValid = true;
+    }
+
+    if (allInputsValid) {
+
+        calculateAge();
+    }
+}
 
 
 circleArrow.addEventListener("click", function (e) {
+    e.preventDefault();
+    validateInput();
 
-    calculateAge();
 });
