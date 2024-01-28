c847e298bcb899f23f07d171462093e20132bcf1
Jessica Rosier <jessica.c.rosier1@gmail.com>
2024-01-27 18:08:48 -0600

    finishing up advice generator

--- advice-generator-app/js/main.js
+++ advice-generator-app/js/main.js
@@ -10,4 +10,41 @@ const getAdvice = async () => {
     }
 };
 
-getAdvice().then((data) => console.log(data.slip.advice));
+getAdvice().then((data) => console.log(data));
+
+async function renderAdviceCard() {
+    const advice = await getAdvice();
+
+    const adviceCard = document.createElement("div");
+    adviceCard.classList.add("advice-card");
+
+    adviceCard.innerHTML = `
+    <div class="d-flex flex-column justify-content-center align-items-center">
+      <p class="advice-id">Advice # ${advice.slip.id}</p>
+      <p class="advice-text text-center p-3">"${advice.slip.advice}"</p>
+      <img src="./images/pattern-divider-desktop.svg" alt="divider">
+      <div class="dice-button">
+      <img src="./images/icon-dice.svg" alt="dice">
+      </div>
+    </div>
+    `
+
+    const diceButton = adviceCard.querySelector(".dice-button");
+    diceButton.addEventListener("click", async () => {
+        adviceCard.remove();
+       await renderAdviceCard();
+    })
+
+    const adviceWrapper = document.querySelector("#advice-wrapper");
+    adviceWrapper.appendChild(adviceCard);
+}
+
+window.addEventListener("load", async () => {
+    await renderAdviceCard();
+})
+
+
+
+
+
+
