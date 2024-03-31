8efc78eed184ebd82c1a6a97832d00fb13227b1b
Jessica Rosier <jessica.c.rosier1@gmail.com>
2024-03-31 11:25:20 -0500

    added search input event listener

--- rest-countries-api/js/main.js
+++ rest-countries-api/js/main.js
@@ -15,10 +15,71 @@ const renderCountries = async () => {
         countries.forEach((country) => {
             const countryCard = buildCountryCard(country);
             countryContainer.appendChild(countryCard);
+
+            countryCard.addEventListener("click", () => {
+                renderSingleCountry(country);
+            });
         });
     } catch (error) {
     }
 };
+
+const renderSingleCountry = (country) => {
+    const countryContainer = document.getElementById("countries-container");
+    countryContainer.innerHTML = "";
+    const backButton = renderBackButton();
+    countryContainer.appendChild(backButton);
+    const singleCountryCard = buildSingleCountryCard(country);
+    countryContainer.appendChild(singleCountryCard);
+}
+
+const renderBackButton = () => {
+    const backButton = document.createElement("button");
+    backButton.classList.add("btn", "btn-outline-white");
+    backButton.innerText = "Back";
+    backButton.addEventListener("click", async () => {
+        const countryContainer = document.getElementById("countries-container");
+        countryContainer.innerHTML = "";
+        await renderCountries();
+    });
+    return backButton;
+}
+
+const buildSingleCountryCard = (country) => {
+    const singleCountryCard = document.createElement("div");
+
+    const singleCountryCardHTML = `
+    <div class="card">
+        <img src="${country.flags.png}" alt="${country.name.common}" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title
+            ">${country.name.common}</h5>
+            <p class="card-text">
+                <strong>Population:</strong> ${country.population}
+            </p>
+            <p class="card-text">
+                <strong>Region:</strong> ${country.region}
+                <strong>Subregion:</strong> ${country.subregion}
+            </p>
+            <p class="card-text">
+                <strong>Capital:</strong> ${country.capital}
+            </p>
+            <p class="card-text">
+                <strong>Top Level Domain:</strong> ${country.tld}
+            </p>
+            <p class="card-text">
+                <strong>Currencies:</strong> ${Object.values(country.currencies).map((currency) => currency.name)}
+            </p>
+            <p class="card-text">
+                <strong>Languages:</strong> ${Object.values(country.languages).map((language) => language)}
+            </p>
+        </div>
+    </div>
+    `;
+    singleCountryCard.innerHTML = singleCountryCardHTML;
+    return singleCountryCard;
+
+}
 
 
 const buildCountryCard = (country) => {
@@ -41,9 +102,12 @@ const buildCountryCard = (country) => {
     return countryCard;
 };
 
+
+
 (async () => {
     await renderCountries();
     toggleTheme();
+    await searchCountry();
 })();
 
 const toggleTheme = () => {
@@ -53,4 +117,23 @@ const toggleTheme = () => {
         html.setAttribute("data-bs-theme", html.getAttribute("data-bs-theme") === "dark" ? "light" : "dark");
     });
 };
+
+
+const searchCountry = async () => {
+    const searchInput = document.getElementById("search");
+ searchInput.addEventListener("input", async (e) => {
+        const searchValue = e.target.value.toLowerCase();
+        const countries = await getCountries();
+        const filteredCountries = countries.filter((country) => country.name.common.toLowerCase().includes(searchValue));
+        const countryContainer = document.getElementById("countries-container");
+        countryContainer.innerHTML = "";
+        filteredCountries.forEach((country) => {
+            const countryCard = buildCountryCard(country);
+            countryContainer.appendChild(countryCard);
+            countryCard.addEventListener("click", () => {
+                renderSingleCountry(country);
+            });
+        });
+    });
+ };
 
